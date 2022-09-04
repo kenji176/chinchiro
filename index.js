cd7dd66183e8a7f99129375365c61c8cbfc04007
sinc <73232388+kenji176@users.noreply.github.com>
2022-09-04 19:06:26 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,8 @@
-/**
- * @name coderun
- * @author YourName
- * @description Describe the basic functions. Maybe a support server link.
- * @version 0.0.1
- */
-
-module.exports = class ExamplePlugin {
-    getName() {
-        return "js runer";
-    }
-    getDescription() {
-        return "Does things with the library";
-    }
-    getVersion() {
-        return "0.0.1";
-    }
-    getAuthor() {
-        return "browser js";
-    }
-    getSettingsPanel() {
-        return ''
-    }
-    onSwitch() {}
-    start() {
-        setInterval(() => {
-            function main() {
+function main() {
                 const codes = document.querySelectorAll("code")
                 for (let i = 0; i < codes.length; i++) {
                     if (codes[i].textContent == '@scthorvath1' || codes[i].className == 'inline' || codes[i].id == 'scpath') {
-                        if (codes[i].childElementCount == 0) {
+                        if (codes[i].childElementCount == 0 && !codes[i].className == 'inline') {
                             codes[i].setAttribute('id', 'scpath');
                             codes[i].insertAdjacentHTML('beforeend', '<div id="script"><div style=display:flex;justify-content:flex-end><button class="run_btn" type="button" style=border-radius:20px; class=run_button>Run</button></div></div>');
                         };
@@ -41,19 +15,7 @@ module.exports = class ExamplePlugin {
                 for (var i = 0; i < btns.length; i++) {
                     btns[i].onclick = (e) => {
                         const code = e.target.parentNode.parentNode.parentNode.textContent.slice(0, -3);
-                        eval(code);
+                        eval(code.replaceAll('console.log', 'BdApi.showToast'));
                     }
                 }
             }
-            main()
-        }, 1000);
-    }
-    stop() {
-        const e = document.getElementById('script');
-        if (e) {
-            e.parentNode.removeChild(e);
-        }
-
-    }
-
-}
